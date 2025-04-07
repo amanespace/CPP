4f53e3233cdf64be56b72d07ecf713371e2d64e9
amanthakur2386 <amanthakur2010.ak@gmail.com>
2025-04-07 18:55:57 +0530

    first commit

---
+++ basicIO.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+    int num1,num2;
+    std::cout<<"Enter the value of num1:\n";
+    std::cin>>num1;
+    std::cout<<"Enter the value of num2:\n";
+    std::cin>>num2;
+    std::cout<<"The sum is: "<<num1+num2<<"\n";
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int main(int argc, char const *argv[])
+{
+    int a=14,b=15;
+    float pi=3.14;
+    char c='d';
+    bool is_true=false;
+    bool is_true_new=true;
+
+    std::cout<<is_true<<"\n"; //Returns 0 for false
+    std::cout<<is_true_new<<"\n"; //Returns 1 for true
+
+    
+
+    return 0;
+}
